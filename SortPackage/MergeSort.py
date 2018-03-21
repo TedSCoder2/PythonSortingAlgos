e6294770874a1b36237adff0d1534189def732a8
TedSCoder <37306976+TedSCoder@users.noreply.github.com>
2018-03-20 20:33:59 -0400

    Added MergeSort file, not completed

---
+++ SortPackage/MergeSort.py
@@ -0,0 +1,16 @@
+'''
+Mergesort program using Python
+main function: merge_sort(mylist)
+input: list of unsorted numbers
+output: list of sorted numbers
+'''
+from SortPackage.SubPackage.validcheck import valid_list
+
+# the main function
+def merge_sort(mylist):
+	if not valid_list(mylist) and not valid_flag: 
+		print("Please input valid list of numbers!")
+		return mylist
+
+	return mylist
+
